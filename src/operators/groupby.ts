35d2853cff55dd7e5b264ad0266709a8b344a759
marcin najder <marcin.najder@comarch.pl>
2016-12-17 23:34:50 +0100

    full LINQ compliant groupby operator

--- src/operators/groupby.ts
+++ src/operators/groupby.ts
@@ -1,45 +1,64 @@
 import {Enumerable} from "../enumerable";
 
-export interface Grouping<TKey,T> extends Iterable<T> {
+export interface IterableGroup<TKey,T> extends Iterable<T> {
     key:TKey
 }
-export function groupby<T,TKey>(source: Iterable<T>, keySelector:(item:T) => TKey) : Iterable<Grouping<TKey,T>>;
-export function groupby<T,TKey,TResult>(source: Iterable<T>, keySelector:(item:T) => TKey, resultSelector:(key:TKey, items:Iterable<T>) => TResult) : Iterable<TResult>;
-export function* groupby<T,TKey,TResult>(source: Iterable<T>, keySelector:(item:T) => TKey, resultSelector?:(key:TKey, items:Iterable<T>) => TResult) : Iterable<TResult>{
-    var map = new Map<TKey, T[]>();
-    var key:TKey, items:T[];
+export interface EnumerableGroup<TKey,T> extends Enumerable<T> {
+    key:TKey
+}
+export function groupby<T,TKey>(source: Iterable<T>, keySelector:(item:T,index:number) => TKey) : Iterable<IterableGroup<TKey,T>>;
+export function groupby<T,TKey,TElement>(source: Iterable<T>, keySelector:(item:T,index:number) => TKey, elementSelector:(item:T) => TElement) : Iterable<IterableGroup<TKey,TElement>>;
+export function groupby<T,TKey,TResult>(source: Iterable<T>, keySelector:(item:T,index:number) => TKey, resultSelector:(key:TKey, items:Iterable<T>) => TResult) : Iterable<TResult>;
+export function groupby<T,TKey,TElement,TResult>(source: Iterable<T>, keySelector:(item:T,index:number) => TKey, elementSelector:(item:T) => TElement, resultSelector:(key:TKey, items:Iterable<TElement>) => TResult) : Iterable<TResult>;
+export function* groupby<T,TKey,TElement,TResult>(source: Iterable<T>, keySelector:(item:T,index:number) => TKey, elementSelector?:(item:T) => TElement, resultSelector?:(key:TKey, items:Iterable<TElement>) => TResult) : Iterable<TResult|IterableGroup<TKey,TElement>>{
+    var map = new Map<TKey, TElement[]>();
+    var key:TKey, element:TElement, items:TElement[];
 
+    if(typeof resultSelector === "undefined"){ // if not all arguments are specified
+        if(typeof elementSelector === "undefined"){ // if only keySelector is specified
+            elementSelector = <any> (x => x);
+        }
+        else { // if elementSelector or resultSelector is specified
+            if(elementSelector.length === 2){ // if elementSelector is a resultSelector
+                resultSelector = <any> elementSelector;
+                elementSelector = <any> (x => x);
+            }
+        }
+    }
+
+    var index = 0;
     for(var item of source){
-        key = keySelector(item);
+        key = keySelector(item, index++);
+        element = elementSelector(item);
         items = map.get(key);
         if(typeof items === "undefined"){
-            map.set(key, [item]);
+            map.set(key, [element]);
         }else{
-            items.push(item);
+            items.push(element);
         }
     }
 
     if(typeof resultSelector === "undefined"){
         for(var [entryKey, entryValue] of map.entries()){
-            yield <any> <Grouping<TKey,T>> {
-                key:entryKey,
-                [Symbol.iterator]: () => entryValue[Symbol.iterator]()
-            }
+            var enumerable = <EnumerableGroup<TKey,TElement>> new Enumerable<TElement>(entryValue);
+            enumerable.key = entryKey;
+            yield enumerable;
         }
     }
     else{
         for(var [entryKey, entryValue] of map.entries()){
-            yield resultSelector(entryKey, entryValue);
+            yield resultSelector(entryKey, new Enumerable<TElement>(entryValue));
         }
     }
 }
 declare module '../enumerable' {
     interface Enumerable<T> {
-        groupby<TKey>(keySelector:(item:T) => TKey) : Enumerable<Grouping<TKey,T>>;
-        groupby<TKey,TResult>(keySelector:(item:T) => TKey, resultSelector:(key:TKey, items:Iterable<T>) => TResult) : Enumerable<TResult>;
-    
+        groupby<TKey>(keySelector:(item:T,index:number) => TKey) : Enumerable<EnumerableGroup<TKey,T>>;
+        groupby<TKey,TElement>(keySelector:(item:T,index:number) => TKey, elementSelector:(item:T) => TElement) : Enumerable<EnumerableGroup<TKey,TElement>>;
+        groupby<TKey,TResult>(keySelector:(item:T,index:number) => TKey, resultSelector:(key:TKey, items:Enumerable<T>) => TResult) : Enumerable<TResult>;
+        groupby<TKey,TElement,TResult>(keySelector:(item:T,index:number) => TKey, elementSelector:(item:T) => TElement, resultSelector:(key:TKey, items:Enumerable<TElement>) => TResult) : Enumerable<TResult>;
     }
 }
-Enumerable.prototype.groupby = function<T,TKey, TResult>(this:Enumerable<T>, keySelector:(item:T) => TKey, resultSelector?:(key:TKey, items:Iterable<T>) => TResult) : Enumerable<TResult>{
-    return new Enumerable<TResult>(groupby(this,keySelector, resultSelector)); 
+Enumerable.prototype.groupby = function<T,TKey,TElement,TResult>(this:Enumerable<T>, keySelector:(item:T,index:number) => TKey, elementSelector?:(item:T) => TElement, resultSelector?:(key:TKey, items:Iterable<TElement>) => TResult) : Enumerable<TResult>{
+     return new Enumerable<TResult>(groupby(this,keySelector, elementSelector, resultSelector)); 
 };
--- test/operators/groupby.ts
+++ test/operators/groupby.ts
@@ -1,11 +1,23 @@
 import * as assert from "assert";
-import { Enumerable, map} from "../../src/index";
+import { Enumerable, EnumerableGroup, groupby} from "../../src/index";
 
 it('groupby', function () {
-    assert.deepEqual(Enumerable.from(["a", "b", "cc", "ddd"])
-        .groupby(x => x.length).map(x => x.key + ":" + Array.from(x).join(",")).toarray(), ["1:a,b", "2:cc", "3:ddd"]);
-    assert.deepEqual(Enumerable.from(["a", "b", "cc", "ddd"])
-        .groupby(x => x.length, (key, items) => key + ":" + Array.from(items).join(",")).toarray(), ["1:a,b", "2:cc", "3:ddd"]);
+    var items = Enumerable.from(["a", "b", "cc", "ddd"]);
+    assert.deepEqual(items.groupby(x => x.length).map(groupToString).toarray(), ["1:a,b", "2:cc", "3:ddd"]);
+    assert.deepEqual(items.groupby((x,index) => index % 2).map(groupToString).toarray(), ["0:a,cc", "1:b,ddd"]);
+    assert.deepEqual(items.groupby(x => x.length, x=> x + x).map(groupToString).toarray(), ["1:aa,bb", "2:cccc", "3:dddddd"]); // using elementSelector
+    assert.deepEqual(items.groupby(x => x.length, (key:number, items:Enumerable<string>) => key + ":" + Array.from(items).join(",")).toarray(), ["1:a,b", "2:cc", "3:ddd"]); // using resultSelector
+
+    assert.deepEqual(items.groupby(
+        x => x.length,                                                  //  keySelector     : (x:string) => number
+        x => x + x,                                                     //  elementSelector : (x:string) => string
+        (key, items) => key + ":" + items.toarray().join(",")           //  resultSelector  : (key:number, items:Enumerable<string>) => string
+    ).toarray(), ["1:aa,bb", "2:cccc", "3:dddddd"]);                    
 });
+
+
+function groupToString<TKey,TElement>(group :EnumerableGroup<TKey,TElement> ){
+    return group.key + ":" + Array.from<TElement>(group).join(",")
+}
 
 export const linq = "GroupBy";
